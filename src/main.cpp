f068304bed22c0f4d9014f4cd5b14137665c9db5
nesci <markgagnon28@gmail.com>
2020-08-24 09:59:30 -0400

    activated second PiR

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,17 @@
 #define COLOR_ORDER GRB
 #define LED_COUNT 5
 #define STAIRS_COUNT 13
-#define BRIGHTNESS 25    // 255 Max
-#define COLOR "FAEBD7"   // HEX Code
-#define DELAY_LEDS 10    // in MS
-#define DELAY_STAIRS 75  // in MS
-#define STAY_ON 5        // in S
+#define BRIGHTNESS 25   // 255 Max
+#define COLOR "FAEBD7"  // HEX Code
+#define DELAY_LEDS 10   // in MS
+#define DELAY_STAIRS 75 // in MS
+#define STAY_ON 5       // in S
 
 // PINS
-// #define PIR_PIN_UP_STAIRS D6
+#define PIR_PIN_UP_STAIRS D6
 #define PIR_PIN_DOWN_STAIRS D5
 #define LED_PIN_1 4
-// #define LED_PIN_2 3
+#define LED_PIN_2 3
 
 CRGB leds[LED_COUNT * STAIRS_COUNT];
 int R;
@@ -26,15 +26,20 @@ int G;
 int B;
 boolean inAnimation = false;
 
-int convert(const char num[]) {
+int convert(const char num[])
+{
   int len = strlen(num);
   int base = 1;
   int temp = 0;
-  for (int i = len - 1; i >= 0; i--) {
-    if (num[i] >= '0' && num[i] <= '9') {
+  for (int i = len - 1; i >= 0; i--)
+  {
+    if (num[i] >= '0' && num[i] <= '9')
+    {
       temp += (num[i] - 48) * base;
       base = base * 16;
-    } else if (num[i] >= 'A' && num[i] <= 'F') {
+    }
+    else if (num[i] >= 'A' && num[i] <= 'F')
+    {
       temp += (num[i] - 55) * base;
       base = base * 16;
     }
@@ -42,15 +47,14 @@ int convert(const char num[]) {
   return temp;
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  // pinMode(PIR_PIN_UP_STAIRS, INPUT);
+  pinMode(PIR_PIN_UP_STAIRS, INPUT);
   pinMode(PIR_PIN_DOWN_STAIRS, INPUT);
 
-  LEDS.addLeds<LED_TYPE, LED_PIN_1, COLOR_ORDER>(leds,
-                                                 LED_COUNT * STAIRS_COUNT);
-  // LEDS.addLeds<LED_TYPE, LED_PIN_2, COLOR_ORDER>(leds, LED_COUNT *
-  // STAIRS_COUNT);
+  LEDS.addLeds<LED_TYPE, LED_PIN_1, COLOR_ORDER>(leds, LED_COUNT * STAIRS_COUNT);
+  LEDS.addLeds<LED_TYPE, LED_PIN_2, COLOR_ORDER>(leds, LED_COUNT * STAIRS_COUNT);
   LEDS.setBrightness(BRIGHTNESS);
 
   std::string color = COLOR;
@@ -66,9 +70,12 @@ void setup() {
   Serial.println((String) "B:" + B);
 }
 
-void goingUp(int r, int g, int b) {
-  for (int i = 0; i < STAIRS_COUNT; i++) {
-    for (int j = 0; j < LED_COUNT; j++) {
+void goingUp(int r, int g, int b)
+{
+  for (int i = 0; i < STAIRS_COUNT; i++)
+  {
+    for (int j = 0; j < LED_COUNT; j++)
+    {
       int currentLedPos = i * LED_COUNT + j;
       leds[currentLedPos] = CRGB(r, g, b);
       LEDS.show();
@@ -78,41 +85,49 @@ void goingUp(int r, int g, int b) {
   }
 }
 
-void goingDown(int r, int g, int b) {
-  for (int i = STAIRS_COUNT - 1; i >= 0; i--) {
-    for (int j = LED_COUNT - 1; j >= 0; j--) {
+void goingDown(int r, int g, int b)
+{
+  for (int i = STAIRS_COUNT - 1; i >= 0; i--)
+  {
+    for (int j = LED_COUNT - 1; j >= 0; j--)
+    {
       int currentLedPos = i * LED_COUNT + j;
-      for (int x = STAIRS_COUNT * LED_COUNT - 1; x >= currentLedPos; x--) {
-        leds[x] = CRGB(r, g, b);
-        LEDS.show();
-      }
+      leds[currentLedPos] = CRGB(r, g, b);
+      LEDS.show();
       FastLED.delay(DELAY_LEDS);
     }
     FastLED.delay(DELAY_STAIRS);
   }
 }
 
-void ledAnimationGoingUp() {
+void ledAnimationGoingUp()
+{
   goingUp(R, G, B);
   FastLED.delay(STAY_ON * 1000);
   goingUp(0, 0, 0);
 }
 
-void ledAnimationGoingDown() {
+void ledAnimationGoingDown()
+{
   goingDown(R, G, B);
   FastLED.delay(STAY_ON * 1000);
   goingDown(0, 0, 0);
 }
 
-void loop() {
+void loop()
+{
   // Upstairs Motion
-  // if (digitalRead(PIR_PIN_UP_STAIRS) == HIGH) {
-  //   Serial.println("Upstairs Motion Detected");
-  //   ledAnimationGoingDown();
-  // }
+  if (digitalRead(PIR_PIN_UP_STAIRS) == HIGH && inAnimation == false)
+  {
+    inAnimation = true;
+    Serial.println("Upstairs Motion Detected");
+    ledAnimationGoingDown();
+    inAnimation = false;
+  }
 
   // Downstairs Motion
-  if (digitalRead(PIR_PIN_DOWN_STAIRS) == HIGH && inAnimation == false) {
+  if (digitalRead(PIR_PIN_DOWN_STAIRS) == HIGH && inAnimation == false)
+  {
     inAnimation = true;
     Serial.println("Downstairs Motion Detected");
     ledAnimationGoingUp();
